e0a8e7fa146d44a374a2e4f4bb7183c48ec30a97
diego <diego_aleo@outook.com>
2020-12-06 17:34:03 -0600

    Se completó el formulario con carrera, falta Datepicker para ingresar fecha de nacimiento

--- app/src/main/java/com/tallercmovil/formulario_ej1/MainActivity2.java
+++ app/src/main/java/com/tallercmovil/formulario_ej1/MainActivity2.java
@@ -2,7 +2,9 @@ package com.tallercmovil.formulario_ej1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.tallercmovil.formulario_ej1.model.Alumno;
@@ -10,6 +12,7 @@ import com.tallercmovil.formulario_ej1.model.Alumno;
 public class MainActivity2 extends AppCompatActivity {
 
     TextView tvNombre, tvApellidos, tvNumCuenta, tvCarrera;
+    ImageView imagCarrera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,7 @@ public class MainActivity2 extends AppCompatActivity {
         tvApellidos = findViewById(R.id.tvApellidos);
         tvNumCuenta = findViewById(R.id.tvNumCuenta);
         tvCarrera = findViewById(R.id.tvCarrera);
+        imagCarrera = findViewById(R.id.imgCarrera);
 
         Bundle bundle = getIntent().getExtras();
 
@@ -34,7 +38,39 @@ public class MainActivity2 extends AppCompatActivity {
 
 
 
-        
+        if(alum.getCarrera().equals("Ingeniería Aeroespacial"))
+            imagCarrera.setImageResource(R.drawable.aeroespacial);
+        else if (alum.getCarrera().equals("Ingeniería Civil"))
+            imagCarrera.setImageResource(R.drawable.civil);
+        else if (alum.getCarrera().equals("Ingeniería Geomática"))
+            imagCarrera.setImageResource(R.drawable.geomatica);
+        else if (alum.getCarrera().equals("Ingeniería Ambiental"))
+            imagCarrera.setImageResource(R.drawable.ambiental);
+        else if (alum.getCarrera().equals("Ingeniería Geofísica"))
+            imagCarrera.setImageResource(R.drawable.geofisica);
+        else if (alum.getCarrera().equals("Ingeniería Geológica"))
+            imagCarrera.setImageResource(R.drawable.geologica);
+        else if (alum.getCarrera().equals("Ingeniería Petrolera"))
+            imagCarrera.setImageResource(R.drawable.petrolera);
+        else if (alum.getCarrera().equals("Ingeniería de Minas y Metalurgía"))
+            imagCarrera.setImageResource(R.drawable.minas);
+        else if (alum.getCarrera().equals("Ingeniería en Computación"))
+            imagCarrera.setImageResource(R.drawable.computacion);
+        else if (alum.getCarrera().equals("Ingeniería Eléctrica Electrónica"))
+            imagCarrera.setImageResource(R.drawable.electrica);
+        else if (alum.getCarrera().equals("Ingeniería en Telecomunicaciones"))
+            imagCarrera.setImageResource(R.drawable.telecom);
+        else if (alum.getCarrera().equals("Ingeniería Industrial"))
+            imagCarrera.setImageResource(R.drawable.industrial);
+        else if (alum.getCarrera().equals("Ingeniería Mecatrónica"))
+            imagCarrera.setImageResource(R.drawable.mecatronica);
+        else if (alum.getCarrera().equals("Ingeniería en Sistemas Biomédicos"))
+            imagCarrera.setImageResource(R.drawable.biomedicos);
+        else if (alum.getCarrera().equals("Ingeniería en Mecánica"))
+            imagCarrera.setImageResource(R.drawable.mecanica);
+
+
+
 
 
 
