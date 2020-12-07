8ced6ab51988cfd4ed77972e4127eb9e2c3a41ad
diego <diego_aleo@outook.com>
2020-12-06 23:02:27 -0600

    Proyecto terminado, con traducciones

--- app/src/main/java/com/tallercmovil/formulario_ej1/MainActivity2.java
+++ app/src/main/java/com/tallercmovil/formulario_ej1/MainActivity2.java
@@ -33,15 +33,15 @@ public class MainActivity2 extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
 
-        Alumno alum = (Alumno )bundle.getSerializable("alumno");
+        Alumno alum = (Alumno) bundle.getSerializable("alumno");
 
         String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
 
-        int anioActual = Integer.parseInt(date.substring(0,4));
-        int mesActual = Integer.parseInt(date.substring(5,7));
-        int diaActual = Integer.parseInt(date.substring(8,10));
+        int anioActual = Integer.parseInt(date.substring(0, 4));
+        int mesActual = Integer.parseInt(date.substring(5, 7));
+        int diaActual = Integer.parseInt(date.substring(8, 10));
 
-        int edad =0;
+        int edad = 0;
 
         if (anioActual > alum.getAnioNac()) {
             edad = anioActual - alum.getAnioNac() - 1;
@@ -49,7 +49,7 @@ public class MainActivity2 extends AppCompatActivity {
 
             if (mesActual > (alum.getMesNac() + 1)) edad++;
 
-            else if (mesActual == (alum.getMesNac()+1)) {
+            else if (mesActual == (alum.getMesNac() + 1)) {
                 if (diaActual >= alum.getDiaNac()) edad++;
             }
 
@@ -65,7 +65,7 @@ public class MainActivity2 extends AppCompatActivity {
 
         String carrera = alum.getCarrera();
 
-        switch(carrera){
+        switch (carrera) {
 
             case "Ingeniería Aeroespacial":
                 imagCarrera.setImageResource(R.drawable.aeroespacial);
@@ -125,11 +125,60 @@ public class MainActivity2 extends AppCompatActivity {
                 imagCarrera.setImageResource(R.drawable.mecanica);
                 break;
 
+            case "Aerospace Engineering":
+                imagCarrera.setImageResource(R.drawable.aeroespacial);
+                break;
+            case "Civil Engineering":
+                imagCarrera.setImageResource(R.drawable.civil);
+                break;
+            case "Geomatics Engineering":
+                imagCarrera.setImageResource(R.drawable.geomatica);
+                break;
+            case "Enviromental Engineering":
+                imagCarrera.setImageResource(R.drawable.ambiental);
+                break;
+            case "Geophysical Engineering":
+                imagCarrera.setImageResource(R.drawable.geofisica);
+                break;
+            case "Petroleum Engineering":
+                imagCarrera.setImageResource(R.drawable.petrolera);
+                break;
+            case "Mining and Metallurgical Engineering":
+                imagCarrera.setImageResource(R.drawable.minas);
+                break;
+
+            case "Computer Engineering":
+                imagCarrera.setImageResource(R.drawable.computacion);
+                break;
+
+            case "Electrical And Electronics Engineering":
+                imagCarrera.setImageResource(R.drawable.electrica);
+                break;
+
+
+            case "Telecommunications Engineering":
+                imagCarrera.setImageResource(R.drawable.telecom);
+                break;
+            case "Mechanical Engineering":
+                imagCarrera.setImageResource(R.drawable.mecanica);
+                break;
+
+            case "Industrial Engineering":
+                imagCarrera.setImageResource(R.drawable.industrial);
+                break;
+
+
+            case "Mechatronics Engineering":
+                imagCarrera.setImageResource(R.drawable.mecatronica);
+                break;
+
+            case "Biomedical Engineering":
+                imagCarrera.setImageResource(R.drawable.biomedicos);
+                break;
+
+
+
+
         }
-
-
-
-
-
     }
 }
